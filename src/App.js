cc9781902ebed7998bc3d6d2643fc877bade5f8e
KHA  DUY <nhokkha345@gmail.com>
2023-03-03 16:36:15 +0700

    Update title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Kha Nguyen</p>
+        <p>Nguyen Huu Duy Kha</p>
       </header>
     </div>
   );
